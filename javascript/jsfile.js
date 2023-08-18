0523816b8285be518e10893aaed6892dd0fc0952
heitornivaldo <125827650+heitornivaldo@users.noreply.github.com>
2023-08-18 10:31:56 -0700

    Teste de horizontes

---
+++ javascript/jsfile.js
@@ -0,0 +1,5 @@
+function menuOnClick() {
+    document.getElementById("menu2-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
